2e12748e9a0f88ab02f8b1c4f6e700421b7af99f
matheuspiment <matheuspiment@hotmail.com>
2017-11-22 05:45:00 -0200

    :art: update style for subjects cards and fix accessibilities bugs

--- assets/js/accessibility.js
+++ assets/js/accessibility.js
@@ -10,6 +10,7 @@ var h6SizeBackup;
 var pWrapperBackup;
 var pDescriptionSizeBackup;
 var pDisciplinaSizeBackup;
+var pCardSizeBackup;
 var pSizeBackup;
 var aBtnSizeBackup;
 var aNavLink;
@@ -22,13 +23,14 @@ function fontSizeBackup() {
   h2SizeBackup = parseInt($( "h2" ).css("font-size"));
   h3SizeBackup = parseInt($( "h3" ).css("font-size"));
   h3TitleSizeBackup = parseInt($( "h3.title" ).css("font-size"));
-  h4SizeBackup = parseInt($( "h4" ).css("font-size"));
+  h4SizeBackup = parseFloat($( "h4" ).css("font-size"));
   h5ASizeBackup = parseInt($( "h5 a" ).css("font-size"));
   h5SizeBackup = parseInt($( "h5" ).css("font-size"));
   h6SizeBackup = parseInt($( "h6" ).css("font-size"));
 	pWrapperBackup = parseInt($( ".wrapper p" ).css("font-size"));
   pDescriptionSizeBackup = parseInt($( "p.description" ).css("font-size"));
 	pDisciplinaSizeBackup = 18.2;
+	pCardSizeBackup = 18.2;
   pSizeBackup = parseInt($( "p" ).css("font-size"));
   aBtnSizeBackup = parseInt($( "a.btn" ).css("font-size"));
 	aNavLinkBackup = parseInt($( ".sidebar a.nav-link" ).css("font-size"));
@@ -36,6 +38,7 @@ function fontSizeBackup() {
   iSizeBackup = parseInt($( "i" ).css("font-size"));
   alertSizeBackup = parseInt($( ".alert.alert-info" ).css("font-size"));
 	listSizeBackup = 18.2;
+
 
 }
 
@@ -67,7 +70,7 @@ function fontSize( option ) {
     var h2Size = parseInt($( "h2" ).css("font-size"));
     var h3Size = parseInt($( "h3" ).css("font-size"));
     var h3TitleSize = parseInt($( "h3.title" ).css("font-size"));
-    var h4Size = parseInt($( "h4" ).css("font-size"));
+    var h4Size = parseFloat($( "h4" ).css("font-size"));
     var h5ASize = parseInt($( "h5 a" ).css("font-size"));
     var h5Size = parseInt($( "h5" ).css("font-size"));
     var h6Size = parseInt($( "h6" ).css("font-size"));
@@ -75,6 +78,7 @@ function fontSize( option ) {
 		var pWrapper = parseInt($( ".wrapper p" ).css("font-size"));
 		var pDescriptionSize = parseInt($( "p.description" ).css("font-size"));
 		var pDisciplinaSize = parseFloat($( "p.description" ).css("font-size"));
+		var pCardSize = parseFloat($( ".card p" ).css("font-size"));
     var aBtnSize = parseInt($( "a.btn" ).css("font-size"));
 		var aNavLink = parseInt($( ".sidebar a.nav-link" ).css("font-size"));
     var aSize = parseInt($( "a" ).css("font-size"));
@@ -92,6 +96,7 @@ function fontSize( option ) {
     h6Size = h6Size + 1 + "px";
     pDescriptionSize = pDescriptionSize + 1 + "px";
 		pDisciplinaSize = pDisciplinaSize + 1 + "px";
+		pCardSize = pCardSize + 1 + "px";
     pSize = pSize + 1 + "px";
     aBtnSize = aBtnSize + 1 + "px";
 		aNavLink = aNavLink + 1 + "px";
@@ -112,6 +117,7 @@ function fontSize( option ) {
     h6Size = h6Size - 1 + "px";
     pDescriptionSize = pDescriptionSize - 1 + "px";
 		pDisciplinaSize = pDisciplinaSize - 1 + "px";
+		pCardSize = pCardSize - 1 + "px";
     pSize = pSize - 1 + "px";
     aBtnSize = aBtnSize - 1 + "px";
 		aNavLink = aNavLink - 1 + "px";
@@ -122,23 +128,24 @@ function fontSize( option ) {
 		listSize = listSize - 1 + "px";
     font--;
   } else if(option == 0) {
-    h2Size = h2SizeBackup + "px";
-    h3Size = h3SizeBackup + "px";
-    h3TitleSize = h3TitleSizeBackup + "px";
-    h4Size = h4SizeBackup + "px";
-    h5ASize = h5ASizeBackup + "px";
-    h5Size = h5SizeBackup + "px";
-    h6Size = h6SizeBackup + "px";
-    pDescriptionSize = pDescriptionSizeBackup + "px";
-    pSize = pSizeBackup + "px";
-		pDisciplinaSize = pDisciplinaSizeBackup + "px";
-    aBtnSize = aBtnSizeBackup + "px";
-		aNavLink = aNavLinkBackup + "px";
-    aSize = aSizeBackup + "px";
-    iSize = iSizeBackup + "px";
-    alertSize = alertSizeBackup + "px";
-		pWrapper = pWrapperBackup + "px";
-		listSize = listSizeBackup + "px";
+    h2Size = h2Size - font + "px";
+    h3Size = h3Size - font + "px";
+    h3TitleSize = h3TitleSize - font + "px";
+    h4Size = h4Size - font + "px";
+    h5ASize = h5ASize - font + "px";
+    h5Size = h5Size - font + "px";
+    h6Size = h6Size - font + "px";
+    pDescriptionSize = pDescriptionSize - font + "px";
+		pCardSize = pCardSize - font + "px";
+    pSize = pSize - font + "px";
+		pDisciplinaSize = pDisciplinaSize - font + "px";
+    aBtnSize = aBtnSize - font + "px";
+		aNavLink = aNavLink - font + "px";
+    aSize = aSize - font + "px";
+    iSize = iSize - font + "px";
+    alertSize = alertSize - font + "px";
+		pWrapper = pWrapper - font + "px";
+		listSize = listSize - font + "px";
     font = 0;
   }
 
@@ -153,6 +160,7 @@ function fontSize( option ) {
 	$('.wrapper p').css({'font-size':pWrapper});
   $('p.description').css({'font-size':pDescriptionSize});
 	$('#subjects p.description').css({'font-size':pDisciplinaSize});
+	$('#subjects .card p').css({'font-size':pCardSize});
   $('a').css({'font-size':aSize});
   $('a.btn').css({'font-size':aBtnSize});
 	$('.sidebar a.nav-link').css({'font-size':aNavLink});
